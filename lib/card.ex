be0e050e90f2e1c65c0a472c28e111a305c08ec1
Julio Aguilar <julio.aguilar9928@gmail.com>
2023-02-23 10:28:49 -0600

    update game card whit the given selected card

--- lib/card.ex
+++ lib/card.ex
@@ -1,32 +1,28 @@
 defmodule MemoryGame.Card do
-  @enforce_keys [:id]
-  defstruct [:id, :flipped_by, flipped: false]
+  @enforce_keys [:id, :pair_of]
+  defstruct [:id, :pair_of, :flipped_by, flipped: false]
 
   alias __MODULE__
 
-  @spec new :: [%Card{}]
   @doc """
   Create a pair of game cards
   """
+  @spec new :: [%Card{}]
   def new() do
-    UUID.uuid1()
-    |> new()
-    |> create_card_pair()
+    first_card_id = UUID.uuid1()
+    second_card_id = UUID.uuid1()
+
+    card1 = Card.new(first_card_id, second_card_id)
+    card2 = Card.new(second_card_id, first_card_id)
+
+    [card1, card2]
   end
 
   @doc """
-  Create a card with the given `id`
+  Create a card with the given `id` and the given `pair_of` id card
   """
-  @spec new(id :: String.t()) :: %Card{}
-  def new(id) do
-    %Card{id: id}
-  end
-
-  @doc """
-  Create a game card pair with the given `card`
-  """
-  @spec create_card_pair(card :: %Card{}) :: [%Card{}]
-  def create_card_pair(card) do
-    [card, card]
+  @spec new(id :: String.t(), pair_of :: String.t()) :: %Card{}
+  def new(id, pair_of) do
+    %Card{id: id, pair_of: pair_of}
   end
 end
--- lib/game.ex
+++ lib/game.ex
@@ -1,19 +1,52 @@
 defmodule MemoryGame.Game do
-  defstruct [:cards, :winner, :scores]
   @enforce_keys [:cards]
+  defstruct [:cards, :winner, :scores, attempt: 0]
 
   alias MemoryGame.Game
   alias MemoryGame.Card
+  alias MemoryGame.Player
 
-  def new(size) do
+  @doc """
+  Create a game whit the given number of card `pairs`
+  """
+  @spec new(pairs :: number()) :: %Game{}
+  def new(pairs) do
     cards =
-      size
+      pairs
       |> create_cards()
-      |> Enum.reduce(fn cards_list, acc -> cards_list ++ acc end)
+      |> List.flatten()
       |> Enum.shuffle()
-      |> Enum.chunk_every(trunc(size / 2) + 1)
+      |> Enum.chunk_every(trunc(pairs / 2) + 1)
 
     %Game{cards: cards}
+  end
+
+  @spec flip_card(game :: %Game{}, card :: %Card{}, player :: %Player{}) :: %Game{}
+  def flip_card(game, card, player) do
+    game
+    |> update_cards_whit_flipped_card(card, player)
+    |> update_player_attempt()
+  end
+
+  defp update_cards_whit_flipped_card(game, card, player) do
+    new_cards =
+      game.cards
+      |> List.flatten()
+      |> Enum.map(&mark_card_as_flipped(&1, card, player))
+      |> Enum.chunk_every(Enum.count(game.cards))
+
+    %Game{game | cards: new_cards}
+  end
+
+  defp mark_card_as_flipped(game_card, selected_card, player) do
+    case game_card.id == selected_card.id do
+      true -> %Card{game_card | flipped: true, flipped_by: player}
+      false -> game_card
+    end
+  end
+
+  defp update_player_attempt(game) do
+    %Game{game | attempt: game.attempt + 1}
   end
 
   @doc """
